5d5247e6916d11cbacd85de4cf172d76a53dd8b0
Yunus Alkan <alkannyunus@gmail.com>
2020-06-04 14:38:57 +0300

    Create initial workspace with pod init

---
+++ NewsShowcaseTests/NewsShowcaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewsShowcaseTests.swift
+//  NewsShowcaseTests
+//
+//  Created by Yunus Alkan on 4.06.2020.
+//  Copyright © 2020 Yunus Alkan. All rights reserved.
+//
+
+import XCTest
+@testable import NewsShowcase
+
+class NewsShowcaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NewsShowcase' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NewsShowcase
+
+  target 'NewsShowcaseTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'NewsShowcaseUITests' do
+    # Pods for testing
+  end
+
+end
